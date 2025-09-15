afb8a5d21ebf0112af002e2ba817fcb8fce8d21e
r3gulus-4rcturus <muh.lanangzalkifla@gmail.com>
2025-09-15 14:38:46 +0700

    add url routing for the new functions

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from main.views import show_main
+from main.views import create_product, show_main, show_product_by_id, show_xml, show_json, show_json_by_id, show_xml_by_id
 
 appname='main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('show_product/<str:product_id>', show_product_by_id, name='show_product_by_id'),
+    path('create_product/', create_product , name='create_product'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<str:product_id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:product_id>/', show_json_by_id, name='show_json_by_id'),
 ]
