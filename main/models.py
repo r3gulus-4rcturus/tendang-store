3a96350f7d59cb10e230acb2db1cf677384700da
r3gulus-4rcturus <muh.lanangzalkifla@gmail.com>
2025-09-24 11:15:01 +0700

    Connect Product with User models

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
@@ -25,6 +26,9 @@ class Product(models.Model):
     
     stock = models.PositiveIntegerField(default=0)
     brand = models.CharField()
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
     
     def __str__(self):
         return self.name
