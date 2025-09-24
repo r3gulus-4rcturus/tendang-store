576979eb3526940ea919d10a452845242a257b09
r3gulus-4rcturus <muh.lanangzalkifla@gmail.com>
2025-09-24 11:16:08 +0700

    implement login, logout, and register page as well as the authorization for some of the views function.

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import create_product, show_main, show_product_by_id, show_xml, show_json, show_json_by_id, show_xml_by_id
+from main.views import create_product, login_user, logout_user, register, show_main, show_product_by_id, show_xml, show_json, show_json_by_id, show_xml_by_id
 
 appname='main'
 
@@ -11,4 +11,7 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<str:product_id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:product_id>/', show_json_by_id, name='show_json_by_id'),
-]
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+] 
